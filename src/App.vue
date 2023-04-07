b17185de1b619f25c416f208d935af543aaba5db
kiwon1011@naver.com <kiwon.kim@seeroo.co.kr>
2023-04-07 17:28:53 +0900

    feat:axios 통신 성공 코드

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <h1>NH voucher Util 모음</h1>
+  <h1>NH voucher 암호화</h1>
   <p><a href="https://documenter.getpostman.com/view/26725844/2s93RWQrLP#intro">NH Voucher API 명세서</a></p>
   
   <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">올원뱅크 암호화 전</label>
-      <input id="username" type="text" v-model="username">
+      <input id="username" type="text" v-model="preEncrpt">
     </div>
     <div>
-      <label for="password">pw: </label>
-      <input id="password" type="password" v-model="password">
+      <label for="username">올원뱅크 암호화 후</label>
+      <input v-bind:type="textarea" :value="encrypt">
     </div>
+
     <button type="submit">제출</button>
+    <div>{{ encrypt }}</div>
+    
   </form>
 </template>
 
@@ -19,23 +22,33 @@
 import axios from 'axios';
 
 export default {
-  data: function() {
-    return {
-      username: '',
-      password: '',
-    }
+  data ()  {
+    return {encrypt : ["초기값"]}
   },
-  methods: {
+    
+  methods: { 
     submitForm: function() {
+      var vm = this; //eslint-disable-line no-unused-vars
       // event.preventDefault();
-      console.log(this.username);
-      const url = 'https://localhost:29347/aes256/gcm/encrypt';
+      console.log(this.encrypt);
+      this.encrypt = "변경값";
+      console.log(this.encrypt);
+
+      console.log(this.preEncrypt);
+
+      const url = 'http://localhost:29347/aes256/gcm/encrypt';
       const data = {
-        username: this.username,
+        data: this.preEncrpt,
       }
+
       axios.post(url, data)
-        .then(function(response) {
+        .then(function(response) {  
+          console.log("axios 통신 성공!");
           console.log(response);
+          console.log(response.data);
+          vm.encrypt = response.data;
+          
+          console.log(response.data);
         })
         .catch(function(error) {
           console.log(error);
