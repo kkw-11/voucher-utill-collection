6c58c405a2ee5020e65b2caca4aa50f4ec1f6cbb
kiwon1011@naver.com <kiwon.kim@seeroo.co.kr>
2023-04-07 17:47:32 +0900

    fix:데이터 바인딩 vue3SetUp 함수로 변경

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <h1>NH voucher 암호화</h1>
+  <h1>NH voucher 암복호화 테스트</h1>
   <p><a href="https://documenter.getpostman.com/view/26725844/2s93RWQrLP#intro">NH Voucher API 명세서</a></p>
   
   <form v-on:submit.prevent="submitForm">
     <div>
-      <label for="username">올원뱅크 암호화 전</label>
-      <input id="username" type="text" v-model="preEncrpt">
+      <label>올원뱅크 암호화 전</label>
+      <input id="username" type="text" v-model="preEncrypt">
     </div>
     <div>
-      <label for="username">올원뱅크 암호화 후</label>
-      <input v-bind:type="textarea" :value="encrypt">
+      <label> 올원뱅크 암호화 후</label>
+      <input type="textarea" :value="encrypt">
     </div>
 
     <button type="submit">제출</button>
@@ -17,44 +17,35 @@
     
   </form>
 </template>
-
-<script>
+<script setup>
 import axios from 'axios';
+import { ref } from 'vue';
 
-export default {
-  data ()  {
-    return {encrypt : ["초기값"]}
-  },
-    
-  methods: { 
-    submitForm: function() {
-      var vm = this; //eslint-disable-line no-unused-vars
-      // event.preventDefault();
-      console.log(this.encrypt);
-      this.encrypt = "변경값";
-      console.log(this.encrypt);
+const encrypt = ref("초기값");
+const preEncrypt = ref('');
 
-      console.log(this.preEncrypt);
+const submitForm = () => {
+  console.log(encrypt);
+  encrypt.value = "변경값";
+  console.log(encrypt);
+  console.log(preEncrypt);
 
-      const url = 'http://localhost:29347/aes256/gcm/encrypt';
-      const data = {
-        data: this.preEncrpt,
-      }
-
-      axios.post(url, data)
-        .then(function(response) {  
-          console.log("axios 통신 성공!");
-          console.log(response);
-          console.log(response.data);
-          vm.encrypt = response.data;
-          
-          console.log(response.data);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    }
+  const url = 'http://localhost:29347/aes256/gcm/encrypt';
+  const data = {
+    data: preEncrypt.value
   }
+
+  axios.post(url, data)
+    .then(function(response) {  
+      console.log("axios 통신 성공!");
+      console.log(response);
+      console.log(response.data);
+      encrypt.value = response.data;
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 </script>
 <style>
